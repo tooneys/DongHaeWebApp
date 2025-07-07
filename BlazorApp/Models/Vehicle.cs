5b47d62a5c89e64227028a7ab1420c5f569e5e5f
Tooney <Tooney@DESKTOP-TRO4RFJ>
2025-07-07 18:05:13 +0900

    20250707 차량일지 추가

---
+++ BlazorApp/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class Vehicle
+    {
+        public int SEQ { get; set; }
+        [Required(ErrorMessage = "등록일자를 입력하세요.")]
+        public DateTime? DT_COMP { get; set; }
+        [Required(ErrorMessage = "사원코드를 입력하세요.")]
+        public string CD_EMP { get; set; }
+        [Required(ErrorMessage = "차량번호를 입력하세요.")]
+        public string NO_CAR { get; set; }
+        public string TX_AREA { get; set; }
+        public decimal VL_BEFORE { get; set; }
+        public decimal VL_AFTER { get; set; }
+        public decimal VL_DISTANCE { get; set; }
+        public decimal VL_FUEL { get; set; }
+    }
+}
